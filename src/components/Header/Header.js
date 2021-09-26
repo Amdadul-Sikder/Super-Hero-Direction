e58f0f25df9fe6a4049e467a4a225329f098e606
Amdadul Sikder <amdadulsikder3@gmail.com>
2021-09-26 22:01:38 +0600

    member added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <div className="header">
             <h1>Let's create our awesome developers team</h1>
             <h6>The main purpose of this team should be productive and efficient code.</h6>
-            <h3>Budget: Around 2,00,000.00</h3>
+            <h3>Budget: Around 2,00,000 Tk</h3>
         </div>
     );
 };
